26196524740541815f4c66e4286fd117c6256c39
khadija <khadijamahanga@y7mail.com>
2020-12-15 16:06:06 +0300

    finish function

--- chalicelib/service.py
+++ chalicelib/service.py
@@ -22,7 +22,8 @@ def post_sensor(sensor):
     response = requests.post(f"{settings.SENSORS_AFRICA_API}/v2/sensors/",
     data=sensor,
     headers={"Authorization": f"Token {settings.SENSORS_AFRICA_AUTH_TOKEN}"})
-    return
+    if response.ok:
+        return response.json()['id']
 
 def post_sensor_type(sensor_type):
     response = requests.post(f"{settings.SENSORS_AFRICA_API}/v2/sensor_types/",
@@ -30,6 +31,18 @@ def post_sensor_type(sensor_type):
     headers={"Authorization": f"Token {settings.SENSORS_AFRICA_AUTH_TOKEN}"})
     if response.ok:
         return response.json()['id']
+
+def post_sensor_data(data, node_uid):
+    response = requests.post(f"{settings.SENSORS_AFRICA_API}/v1/push-sensor-data/",
+    json=data,
+    headers={
+        "Authorization": f"Token {settings.SENSORS_AFRICA_AUTH_TOKEN}",
+        "X_SENSOR": str(node_uid)
+        }
+    )
+    if response.ok:
+        return response.json()
+    return []
 
 def get_sensors_africa_nodes():
     response = requests.get(f"{settings.SENSORS_AFRICA_API}/v1/node/", auth=(settings.SENSORS_AFRICA_USER, settings.SENSORS_AFRICA_PASSWORD))
@@ -54,8 +67,7 @@ def get_sensors_africa_locations():
 def get_airqo_node_sensors_data(node_id):
     response = requests.get("https://thingspeak.com/channels/{}/feeds.json".format(node_id))
     if response.ok:
-        result = response.json()
-
+        return response.json()
     return []
 
 #def run();
@@ -89,16 +101,32 @@ with open("./channels.json") as data:
         else:
             airqo_node = post_node(node={"uid": channel["id"], 'owner': settings.OWNER_ID, 'location': location})
 
+
+        channel_data = get_airqo_node_sensors_data(channel["id"])
         # aiqo channel result has 4 sensors data that we need
         # field1- Sensor1 PM2.5_CF_1_ug/m3, 
-        # field2 -Sensor1 PM2.5_CF_1_ug/m3, 
+        # field2 -Sensor1 PM10_CF_1_ug/m3, 
         # field3 - Sensor2PM2.5_CF_1_ug/m3, 
         # field4 - Sensor2 PM10_CF_1_ug/m3
+        value_type = ["P2", "P1", "P2", "P1"]
         for i in range (1, 5):
-            post_sensor({
+            sensor_id = post_sensor({
                 "node": airqo_node,
                 "pin": str(i),
                 "descriptiion": "",
                 "sensor_type": 1,
                 "public": True
             })
+            
+        sensor_data_values = []
+        for feed in channel_data["feeds"]:
+            sensor_data_values.append({
+                "created": feed["created_at"],
+                "modified":feed["created_at"],
+                "value": float(feed["field{}".format(str(i))]),
+                "value_type": value_type[i-1]
+            })
+
+        post_sensor_data(sensor_data_values, airqo_node)
+
+
